467307b463dc4a52aa23f1023bf4b2e531d44859
dsibbs <dsibbliesjr@gmail.com>
2025-06-11 19:24:08 -0400

    Case sensitive fix

--- client/src/pages/create-party.tsx
+++ client/src/pages/create-party.tsx
@@ -8,7 +8,7 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { AddressAutocomplete } from "@/components/AddressAutocomplete";
+import { AddressAutocomplete } from "@/components/AddressAutoComplete";
 import { insertPartySchema } from "@shared/schema";
 import { apiRequest, queryClient } from "@/lib/queryClient";
 import { useToast } from "@/hooks/use-toast";
